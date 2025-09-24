bc850e1029d749634ef0cd7b3c19fcc485a48b89
anugrahvibi <anugrahvibi07@gmail.com>
2025-09-24 21:22:24 +0530

    basic dashboard added

---
+++ app/Components/Dashboard.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="h-screen w-40 p-2 md:p-5 bg-black-200 fixed border border-gray-100">
+        <button className="">Expenditure</button>
+    </div>
+  )
+}
+
+export default Dashboard
--- app/section/WorkSpace.tsx
+++ app/section/WorkSpace.tsx
@@ -1,9 +1,11 @@
+import Dashboard from "../Components/Dashboard";
 import Header from "../Components/Header";
 
 export default function WorkSpace() {
   return (
     <div className="WorkSpace">
         <Header/>
+        <Dashboard/>
     </div>
   );
 }
